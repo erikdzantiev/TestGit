20586da7de7ddcd0baecc6ed303d20756243e4b0
erikdzantiev <81524099+erikdzantiev@users.noreply.github.com>
2021-04-15 10:09:44 +0300

    удалили функцию
    удалили функцию для демонстрации проекта

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -8,10 +8,6 @@ namespace TestGit
 {
     class Program
     {
-        static void print()
-        {
-            Console.WriteLine(" nj aeyrwbz!");
-        }
         static void Main(string[] args)
         {
             Console.WriteLine(" Hello World! ");
